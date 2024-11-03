0c746fed9bafb41aac337192229fcd4296c2d6f0
william-cass-wright <william.cass.wright@gmail.com>
2024-11-02 23:19:23 -0700

    api calls and fallback

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
